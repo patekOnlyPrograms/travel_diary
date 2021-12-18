fb4646771869c41144ac453832354643b5058770
Susruth <up945169@myport.ac.uk>
2021-12-18 20:11:21 +0000

    Routing works

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -6,7 +6,30 @@ class registerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('Registration Page'),
+      ),
+      body: Column(
+        children: const <Widget>[
+          Padding(
+          padding: EdgeInsets.all(8),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your email address'
+            ),
+          ),
+        ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your password' 
+                ),
+            ),
+          )
+        ],
       ),
     );
   }
