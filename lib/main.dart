f2462a2d1bcfd77051695fedf1b3f26fa4e25322
Susruth <up945169@myport.ac.uk>
2021-12-18 00:29:26 +0000

    Added login and home screen

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'TextBoxes.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Travel Diary'),
+        ),
+        body: Column(children: [
+            LoginPage()
+        ],
+        ),
+      ),
+    );
+  }
+}
+
