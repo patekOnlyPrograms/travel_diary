214a6980b104419df4f712fdb811154487c2ba75
Susruth <up945169@myport.ac.uk>
2021-12-19 23:18:15 +0000

    Added Google maps

--- lib/mapAndMainScreen.dart
+++ lib/mapAndMainScreen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({ Key? key }) : super(key: key);
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+    late GoogleMapController mapController;
+    final LatLng _center = const LatLng(45.521563, -122.677433);
+
+    void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center
+          ),
+        ),
       ),
     );
   }
