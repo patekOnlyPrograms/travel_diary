828dbd941e25e9af32197399fe8361dac82c3c8b
Susruth <up945169@myport.ac.uk>
2022-01-20 02:03:30 +0000

    Added DialogBox for location access

--- lib/mapAndMainScreen.dart
+++ lib/mapAndMainScreen.dart
@@ -25,10 +25,31 @@ class _MainScreenState extends State<MainScreen> {
     return MaterialApp(
       home: Scaffold(
         body: GoogleMap(
+          zoomControlsEnabled: false,
           onMapCreated: _onMapCreated,
+          zoomGesturesEnabled: true,
+          rotateGesturesEnabled: true,
           initialCameraPosition: CameraPosition(
             target: _center
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.location_on_sharp),
+          onPressed: (){
+            showDialog(context: context, builder: (context) => AlertDialog(
+              title: Text("Location Requied"),
+              content: Text("Do you want to turn on location for tracking?"),
+              actions: [
+                TextButton(
+                  onPressed: (){}, 
+                  child: Text("Yes")
+                ),
+                TextButton(
+                  onPressed: (){}, 
+                  child: Text("No"))
+              ],
+            ));
+          },
         ),
       ),
     );
