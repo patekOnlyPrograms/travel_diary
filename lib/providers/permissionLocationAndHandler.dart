4487507624ef8cbb690b9648557347aa784909db
Susruth <up945169@myport.ac.uk>
2022-02-02 11:52:13 +0000

    Please commit

---
+++ lib/providers/permissionLocationAndHandler.dart
@@ -0,0 +1,19 @@
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:flutter/material.dart';
+// import 'package:location/location.dart';
+
+// class locationAndHan extends StatefulWidget {
+//   const locationAndHan({ Key? key }) : super(key: key);
+
+//   @override
+//   _locationAndHanState createState() => _locationAndHanState();
+// }
+
+// class _locationAndHanState extends State<locationAndHan> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: ,
+//     );
+//   }
+// }
