47f54903a4e1b870eb587d0cfada76448ad1c847
Susruth <up945169@myport.ac.uk>
2022-02-06 17:49:08 +0000

    Finally got permissions working

--- lib/mapAndMainScreen.dart
+++ lib/mapAndMainScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:location/location.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -10,78 +8,82 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final Permission _locationPerm = Permission.location;
-  PermissionStatus _locationPermStatus  = PermissionStatus.denied;
-  
   
   late GoogleMapController mapController;
     
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     }
-  
-  @override
-  void initState(){
-    super.initState();
 
-    _listenForPermissionStatus();
+  final Location locationStuff = Location();
+
+  PermissionStatus? _permissionGranted;
+  bool? _serviceEnabled;
+
+  Future<void> _checkPermissions() async{
+    final PermissionStatus permissionGrantedResult =
+    await locationStuff.hasPermission();
+    setState(() {
+      _permissionGranted = permissionGrantedResult;
+    });
   }
 
-  void _listenForPermissionStatus() async{
-    final status = await _locationPerm.status;
-    setState(() => _locationPermStatus = status);
+  Future<void> _requestPermissions() async {
+    if (_permissionGranted != PermissionStatus.granted) {
+      final PermissionStatus permissionRequestedResult = await
+      locationStuff.requestPermission();
+      setState(() {
+        _permissionGranted = permissionRequestedResult;
+      });
+    }
   }
+
+  Future<void> _checkService() async{
+    final bool serviceEnabledResult = await locationStuff.serviceEnabled();
+    setState(() {
+      _serviceEnabled = serviceEnabledResult;
+    });
+  }
+
+  Future<void> _requestService() async{
+    if(_serviceEnabled == true){
+      return;
+    }
+    final bool serviceRequestedResult = await locationStuff.requestService();
+    setState(() {
+      _serviceEnabled = serviceRequestedResult;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(children: <Widget>[
-          GoogleMap(
+        body:
+        GoogleMap(
             mapType: MapType.hybrid,
             zoomControlsEnabled: false,
             onMapCreated: _onMapCreated,
             zoomGesturesEnabled: true,
             rotateGesturesEnabled: true,
-            initialCameraPosition: const CameraPosition(target: LatLng(50.7934166, -1.0904852))
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: FloatingActionButton(
-              child: const Icon(Icons.location_on_sharp),
-              onPressed: (){
-                //From here the app should first check is location is enabled if it is not then it should
-                //open up the dialog box to get location then the user can press outside of the box to get location
-                //once location is turned on the floatingActionbutton should change to location tracking
-                requestPermission(_locationPerm);
-                checkServiceStatus(context, _locationPermStatus as PermissionWithService);
-              },
-          ),
-          )
-        ],)
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(50.7934166, -1.0904852),
+              zoom: 15
+            ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text("Start Tracking"),
+          icon: const Icon(Icons.location_on_sharp),
+          onPressed: (){
+            _permissionGranted == PermissionStatus.granted
+                ? null
+                : _requestPermissions();
+            _serviceEnabled == true ? null : _requestService();
+          },
+        ),
       ),
     );
-  }
-
-  void checkServiceStatus(BuildContext context,PermissionWithService permission) async{
-    // ScaffoldMessenger.of(context).showSnackBar(
-    //   SnackBar(content: Text((await permission.serviceStatus).toString()))
-    // );
-    Fluttertoast.showToast(
-      msg: (permission.serviceStatus).toString(),
-      gravity: ToastGravity.BOTTOM,
-
-    );
-  }
-
-  Future<void> requestPermission(Permission permission) async{
-    final status = await permission.request();
-
-    setState(() {
-      print(status);
-      _locationPermStatus = status;
-      print(_locationPermStatus);
-    });
   }
 }
---
+++ lib/providers/locationService.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class locationService extends StatefulWidget {
+  const locationService({Key? key}) : super(key: key);
+
+  @override
+  _locationServiceState createState() => _locationServiceState();
+}
+
+class _locationServiceState extends State<locationService> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
