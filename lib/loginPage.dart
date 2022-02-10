bee7e43c4b0d97f3f40a692e800c6ddcfeea0015
Susruth <up945169@myport.ac.uk>
2022-02-10 14:03:33 +0000

    Adding flutter email and password

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: use_key_in_widget_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_diary/mapAndMainScreen.dart';
 import 'package:travel_diary/registerPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 //rewriting to allow verfication
 //uses keys to keep track of the state and verification
@@ -15,6 +17,10 @@ class _LoginPageState extends State<LoginPage> {
   //create a boolean for obsureText because stupid TEXTFIELDFORM DOESN'T FUCKING HAVE OBSURE TEXT :(
   
   bool obsureFuckingText = true;
+
+  //firebase Cloud
+  final _auth = FirebaseAuth.instance;
+
   //creating a key to allow validation and is created globably
   final _formKey = GlobalKey<FormState>();
   @override
@@ -59,19 +65,19 @@ class _LoginPageState extends State<LoginPage> {
                 if(value == null || value.isEmpty){
                   return 'Please enter some text';
                 }
-                },
+                RegExp regexPassword = new RegExp(r'^.{6,}$');
+                if(!regexPassword.hasMatch(value)){
+                  return 'Please enter a password that has 6 characers';
+                }
+                return null;
+              },
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 15),
               child: ElevatedButton(
                 onPressed: (){
-                  if(_formKey.currentState!.validate() && _formKey.currentState!.validate()){
-                      Navigator.push(
-                        context, 
-                        MaterialPageRoute(builder: (context) =>  MainScreen())
-                    );
-                  }
+                  //this is where login in to firebase function will go
                 }, 
                 // ignore: prefer_const_constructors
                 child: Text('Submit'),
@@ -105,4 +111,10 @@ class _LoginPageState extends State<LoginPage> {
       )
     );
   }
+}
+
+//login function to ask firebase to login
+
+void signIn(String email, String password) async {
+  if(_)
 }
