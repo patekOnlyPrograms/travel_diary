0aaf0ffae1ed5d71cc62666a7674dfd0accc384f
Susruth <up945169@myport.ac.uk>
2022-02-18 14:31:03 +0000

    reverted back to working code

---
+++ lib/models/UserModel.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: non_constant_identifier_names
+
+class userModelFirebase{
+  String? uid;
+  String? Email;
+  String? FirstName;
+  String? LastName;
+  String? Username;
+  String? Password;
+
+  //UserModel constructor
+
+  userModelFirebase({this.uid,this.Email,this.FirstName,this.LastName,this.Username,this.Password});
+
+  //data from firebase, parse from JSON basically
+  
+}
